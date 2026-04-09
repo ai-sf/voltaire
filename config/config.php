08433f0b37e8f3ed23615613415749dbb84b46af
Dario Zarcone <dariozarcone@gmail.com>
2026-04-09 10:08:01 +0200

    Moved the env variables loading to config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,14 @@
 <?php
 
+// Load environment variables from project root .env (if present)
+if (file_exists(dirname(__DIR__) . '/.env')) {
+    try {
+        \Dotenv\Dotenv::createImmutable(dirname(__DIR__))->safeLoad();
+    } catch (Throwable $e) {
+        // ignore dotenv loading errors
+    }
+}
+
 $db = require __DIR__ . '/_db.php';
 $routes = require __DIR__ . '/_routes.php';
 $app = require __DIR__ . '/_app.php';
--- index.php
+++ index.php
@@ -7,16 +7,6 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
-
-// Load environment variables from .env (if present)
-if (file_exists(__DIR__ . '/.env')) {
-	try {
-		\Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
-	} catch (Throwable $e) {
-		// ignore dotenv loading errors
-	}
-}
-
 global $config;
 
 $config = require 'config/config.php';
